e4ca1163c6c01cd8d5fc065ae6b58167c2ea4464
Aruna <johndoe@example.com>
2020-07-31 12:47:17 -0700

    pom.xml foe SeleniumFrameWork configured for RegressionTest andjenkins

--- src/test/java/com/TekArc/Framework/frameWork_testCases/LoginSFDC.java
+++ src/test/java/com/TekArc/Framework/frameWork_testCases/LoginSFDC.java
@@ -5,7 +5,7 @@ import com.TekArc.Framework.Utilities.TestBase;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -64,18 +64,22 @@ public class LoginSFDC extends TestBase {
 		driver.get(oCons.getAppURL());
 		WebElement username = driver.findElement(By.xpath("//input[@id='username']"));
 		WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
+		
 		username.clear();
+		
 		oBroUti.waitForElementVisible(driver, username, oCons.WAIT_COMMON);
 		oBroUti.ufSendKeys(driver, username, user_name, oCons.WAIT_COMMON);
 		oBroUti.ufSendKeys(driver, password, pass, oCons.WAIT_COMMON);
 		Pologin.loginTAcc();
-		WebElement errorMassage = driver.findElement(By.xpath("//div[@id='error']"));
-		oBroUti.waitForElementVisible(driver, errorMassage, oCons.WAIT_COMMON);
-		String errmsg = errorMassage.getText();
-		if (errmsg.contains("Please check your username and password. If you still can't log in,"
-				+ " contact your Salesforce administrator.")) {
-			System.out.println("Error for wrong UserName and PassWrd is validated");
+		WebElement actualMassage = driver.findElement(By.xpath("//div[@id='error']"));
+		oBroUti.waitForElementVisible(driver, actualMassage, oCons.WAIT_COMMON);
+		String exceptedMessage = "Please check your username and password. If you still can't log in, contact your Salesforce administrator.";
+	
+		if (actualMassage.equals(exceptedMessage)){
+			System.out.println("success");
+		}
+		
 		}
 	}
 
-}
+
